8b0236f18856eda2f7d8715fddd0ee11b05c66bf
pradudev <pradeepchouhan1986@gmail.com>
2014-10-16 15:47:23 +0530

    Initial Setup

---
+++ code/data/automobiles.js
@@ -0,0 +1,88 @@
+﻿[
+   {
+       "src": "images/photos/thumb/automobiles/abc.jpg",
+       "alt": "images/photos/large/automobiles/abc.jpg",
+       "desc": {
+           "0": ""
+       }
+   },
+   {
+       "src": "images/photos/thumb/automobiles/2.jpg",
+       "alt": "images/photos/large/automobiles/2.jpg",
+       "desc": {
+           "0": ""
+       }
+   },
+   {
+       "src": "images/photos/thumb/automobiles/3.jpg",
+       "alt": "images/photos/large/automobiles/3.jpg",
+       "desc": {
+           "0": ""
+       }
+   },
+   {
+       "src": "images/photos/thumb/automobiles/4.jpg",
+       "alt": "images/photos/large/automobiles/4.jpg",
+       "desc": {
+           "0": ""
+       }
+   },
+   {
+       "src": "images/photos/thumb/automobiles/5.jpg",
+       "alt": "images/photos/large/automobiles/5.jpg",
+       "desc": {
+           "0": ""
+       }
+   },
+
+   {
+       "src": "images/photos/thumb/automobiles/6.jpg",
+       "alt": "images/photos/large/automobiles/6.jpg",
+       "desc": {
+           "0": ""
+       }
+   },
+   {
+       "src": "images/photos/thumb/automobiles/7.jpg",
+       "alt": "images/photos/large/automobiles/7.jpg",
+       "desc": {
+           "0": ""
+       }
+   },
+   {
+       "src": "images/photos/thumb/automobiles/8.jpg",
+       "alt": "images/photos/large/automobiles/8.jpg",
+       "desc": {
+           "0": ""
+       }
+   },
+   {
+       "src": "images/photos/thumb/automobiles/9.jpg",
+       "alt": "images/photos/large/automobiles/9.jpg",
+       "desc": {
+           "0": ""
+       }
+   },
+   {
+       "src": "images/photos/thumb/automobiles/10.jpg",
+       "alt": "images/photos/large/automobiles/10.jpg",
+       "desc": {
+           "0": ""
+       }
+   },
+
+   {
+       "src": "images/photos/thumb/automobiles/11.jpg",
+       "alt": "images/photos/large/automobiles/11.jpg",
+       "desc": {
+           "0": ""
+       }
+   },
+   {
+       "src": "images/photos/thumb/automobiles/12.jpg",
+       "alt": "images/photos/large/automobiles/12.jpg",
+       "desc": {
+           "0": ""
+       }
+   }
+]
